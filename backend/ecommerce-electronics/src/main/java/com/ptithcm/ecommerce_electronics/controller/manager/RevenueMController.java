f62f0f0d305cb44f8261c0df41f5e829f6ed5d28
NT-Van-Khanh <ntvk137@gmail.com>
2025-08-08 10:20:41 +0700

    chore: add schema for revenue management APIs

--- backend/ecommerce-electronics/src/main/java/com/ptithcm/ecommerce_electronics/controller/manager/RevenueMController.java
+++ backend/ecommerce-electronics/src/main/java/com/ptithcm/ecommerce_electronics/controller/manager/RevenueMController.java
@@ -4,6 +4,7 @@ import com.ptithcm.ecommerce_electronics.dto.ApiResponse;
 import com.ptithcm.ecommerce_electronics.dto.RevenueDTO;
 import com.ptithcm.ecommerce_electronics.enums.TimeUnit;
 import com.ptithcm.ecommerce_electronics.service.core.OrderService;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -20,24 +21,24 @@ public class RevenueMController {
     private OrderService orderService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<RevenueDTO>> getRevenueByTime(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-                                                                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+    public ResponseEntity<ApiResponse<RevenueDTO>> getRevenueByTime(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Schema(example = "2025-07-09") LocalDate from,
+                                                                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Schema(example = "2025-08-09") LocalDate to,
                                                                      @RequestParam TimeUnit unit){
         return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK, orderService.getRevenueByTime(from, to, unit)));
     }
 
     @GetMapping("/product-variant/{id}")
-    public ResponseEntity<ApiResponse<RevenueDTO>> getRevenueByProductVariant( @PathVariable("id") @PositiveOrZero Integer productVariantId,
-                                                                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-                                                                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+    public ResponseEntity<ApiResponse<RevenueDTO>> getRevenueByProductVariant( @PathVariable("id") @PositiveOrZero @Schema(example = "7") Integer productVariantId,
+                                                                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Schema(example = "2025-07-09") LocalDate from,
+                                                                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Schema(example = "2025-08-09") LocalDate to,
                                                                     @RequestParam TimeUnit unit){
         return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK, orderService.getRevenueByProductVariant( productVariantId, from, to, unit)));
     }
 
     @GetMapping("/discount/{id}")
-    public ResponseEntity<ApiResponse<RevenueDTO>> getRevenueByDiscount( @PathVariable("id") @PositiveOrZero Integer discountId,
-                                                                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-                                                                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+    public ResponseEntity<ApiResponse<RevenueDTO>> getRevenueByDiscount( @PathVariable("id") @PositiveOrZero @Schema(example = "1") Integer discountId,
+                                                                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Schema(example = "2025-07-09") LocalDate from,
+                                                                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Schema(example = "2025-08-09") LocalDate to,
                                                                                @RequestParam TimeUnit unit){
         return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK, orderService.getRevenueByDiscount(discountId, from, to, unit)));
     }
